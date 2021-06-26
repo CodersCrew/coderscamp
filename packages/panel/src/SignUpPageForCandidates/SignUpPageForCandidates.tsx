9ee34234a1615c25692ace9242eed4a1392edb3d
Luke_Z <zurawskiu@gmail.com>
2021-06-26 18:03:52 +0200

    Add CodersCampLogo and library with icons. Start work on RWD

--- packages/panel/src/SignUpPageForCandidates/SignUpPageForCandidates.tsx
+++ packages/panel/src/SignUpPageForCandidates/SignUpPageForCandidates.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
-import { Flex, Spacer, Text, Link, Button, Image, Box } from '@chakra-ui/react';
+import { SiGithub } from 'react-icons/si';
+import { Flex, Text, Link, Button, Image} from '@chakra-ui/react';
 
 function SignUpPageForCandidates() {
   return (
-    <Flex direction="column" align="center" justify="center" h="100vh">
-      <Flex direction="column" align="center" w="70%">
-        <Text fontSize="4xl" color="gray.900" lineHeight="92%" letterSpacing="-2.2%">
+    <Flex direction="column" align="center" h="90vh" mt="5rem">
+      <Flex direction="column" align="center" justify="space-evenly" w={['70%', '100%']} h="60%">
+        <Text fontSize={{ sm: '14px', md: '40px', lg: '4xl' }} color="gray.900" lineHeight="92%" letterSpacing="-2.2%">
           Zapisz się na
         </Text>
-        <Image src="../assets/CodersCampLogo/CodersCampLogo.png" alt="CodersCamp logo" />
-        <Flex mt="3rem" direction="column" width="62%">
-          <Text fontFamily="Inter" fontSize="xl" lineHeight="116%" textAlign="center">
+
+        <Image src="../../assets/CodersCampLogo/CodersCampLogo.png" alt="Logo CodersCamp" />
+
+        <Flex direction="column" width="62%">
+          <Text fontSize="xl" lineHeight="116%" textAlign="center">
             Podczas CodersCamp będzie Ci potrzebne konto na portalu GitHub - najpopularniejszym spośród narzędzi
             umożliwiających udostępnianie kodu innym oraz współpracę przy projektach programistycznych.
           </Text>
-          <Text fontSize="xl" lineHeight="116%" textAlign="center" mt="3rem">
+
+          <Text fontSize='xl' lineHeight="116%" textAlign="center" mt="3rem">
             Jeśli nie posiadasz konta na tej platformie{' '}
             <Link href="https://github.com/signup" color="orange.400" isExternal textDecoration="underline">
               kliknij ten link
@@ -23,7 +27,7 @@ function SignUpPageForCandidates() {
           </Text>
         </Flex>
 
-        <Button mt="3rem" variant="solid" backgroundColor="GitHub.500" color="white">
+        <Button colorScheme="GitHub" leftIcon={<SiGithub />} borderRadius="6px" p="1.25rem">
           Zapisz mnie na CodersCamp używając konta GitHub
         </Button>
       </Flex>
