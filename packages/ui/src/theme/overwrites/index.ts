c441c8db5cf4c350f1c024360e276ef71fa955a1
KonradSzwarc <konrad.szwarc.dev@gmail.com>
2021-06-28 22:42:51 +0200

    Add github color and change theme overrides directory structure

--- packages/ui/src/theme/overwrites/index.ts
+++ packages/ui/src/theme/overwrites/index.ts
@@ -1,13 +1,11 @@
 import { extendTheme } from '@chakra-ui/react';
 
-import { colors } from './colors';
+import { foundations } from './foundations';
 import { styles } from './styles';
-import { typography } from './typography';
 
 export const theme = extendTheme({
-  ...typography,
+  ...foundations,
   styles,
-  colors,
   config: {
     initialColorMode: 'light',
     useSystemColorMode: false,
