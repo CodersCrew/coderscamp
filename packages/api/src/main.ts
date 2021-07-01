4caa27ec4ad71d2fd4516cb951fa18be66058cba
Konrad Szwarc <konrad.szwarc.dev@gmail.com>
2021-07-02 01:13:13 +0200

    ci: Setup CD to create multiple environments (#7)

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
+const { PORT } = process.env;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  await app.listen(4000);
+  await app.listen(PORT ? Number(PORT) : 4000);
 }
 
 bootstrap();
