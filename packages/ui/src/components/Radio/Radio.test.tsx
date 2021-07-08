1e23fec2d7e7948e2d6608e0f96505d253825d72
kacper-cyra <kacper.cyranowski@gmail.com>
2021-07-08 21:47:23 +0200

    Adding missing value property in test

--- packages/ui/src/components/Radio/Radio.test.tsx
+++ packages/ui/src/components/Radio/Radio.test.tsx
@@ -7,13 +7,21 @@ describe('Radio', () => {
   const text = 'Radio label';
 
   it('renders correctly in disabled state', () => {
-    render(<Radio disabled>{text}</Radio>);
+    render(
+      <Radio value="" disabled>
+        {text}
+      </Radio>,
+    );
 
     expect(screen.getByLabelText(text)).toBeDisabled();
   });
 
   it('renders correctly in checked state', () => {
-    render(<Radio checked>{text}</Radio>);
+    render(
+      <Radio value="" checked>
+        {text}
+      </Radio>,
+    );
 
     expect(screen.getByLabelText(text)).toBeChecked();
   });
