53de5aa5f454ae80b72d526c3fb12f13906c7273
Asia <ziola.jm@gmail.com>
2021-07-10 23:32:46 +0200

    feat(ui): Create Logo Component (#12)
    Co-authored-by: Joanna <jziola@efinity.com>
    Co-authored-by: KonradSzwarc <konrad.szwarc.dev@gmail.com>

---
+++ packages/ui/src/components/Logo/Logo.test.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import { Logo } from './Logo';
+
+describe('Logo', () => {
+  it('displays correct alt text', () => {
+    render(<Logo />);
+
+    const logo = screen.getByRole('img');
+    expect(logo).toHaveAttribute('alt', 'Logo CodersCamp');
+  });
+});
