55e96e82be173338e5e2a5a727935cdf0fb38e42
kacper-cyra <kacper.cyranowski@gmail.com>
2021-07-10 19:15:13 +0200

    Adjustments

--- packages/ui/src/components/Radio/Radio.tsx
+++ packages/ui/src/components/Radio/Radio.tsx
@@ -8,29 +8,21 @@ export interface RadioProps extends Omit<HTMLChakraProps<'div'>, 'onChange' | 'd
    * Determines radio's and text size.
    */
   size?: RadioSize;
-  /**
-   * Determines state of the radio.
-   */
   checked?: boolean;
-  /**
-   * Determines if input is disabled.
-   */
   disabled?: boolean;
   value: string | number;
 }
 
 export const Radio = forwardRef<RadioProps, 'input'>(
-  ({ value, checked = false, disabled = false, size = 'md', ...props }, ref) => {
-    return (
-      <ChakraRadio
-        colorScheme="brand"
-        ref={ref}
-        value={value}
-        isChecked={checked}
-        isDisabled={disabled}
-        size={size}
-        {...props}
-      />
-    );
-  },
+  ({ value, checked = false, disabled = false, size = 'md', ...props }, ref) => (
+    <ChakraRadio
+      colorScheme="brand"
+      ref={ref}
+      value={value}
+      isChecked={checked}
+      isDisabled={disabled}
+      size={size}
+      {...props}
+    />
+  ),
 );
