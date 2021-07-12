e251f398ee7fb52dd9fd63dfc3e0f620f18d1955
Natalia <natalia.debska9*@gmail.com>
2021-07-12 17:42:40 +0200

    fix tests

--- packages/ui/src/components/NumberInput/NumberInput.test.tsx
+++ packages/ui/src/components/NumberInput/NumberInput.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 
-import { lgStyleProps, mdStyleProps, NumberInput, smStyleProps } from './NumberInput';
+import { NumberInput } from './NumberInput';
 
 describe('NumberInput', () => {
   it('renders correctly in not disabled mode', () => {
@@ -19,31 +19,6 @@ describe('NumberInput', () => {
     const numberInput = screen.getByLabelText('inputNumber');
 
     expect(numberInput).toBeDisabled();
-    expect(numberInput).toBeInTheDocument();
-  });
-
-  it('renders correctly in large size', () => {
-    render(<NumberInput size="lg" aria-label="inputNumber" />);
-
-    const numberInput = screen.getByLabelText('inputNumber');
-
-    expect(numberInput).toHaveStyle(lgStyleProps);
-    expect(numberInput).toBeInTheDocument();
-  });
-  it('renders correctly in medium size', () => {
-    render(<NumberInput size="md" aria-label="inputNumber" />);
-
-    const numberInput = screen.getByLabelText('inputNumber');
-
-    expect(numberInput).toHaveStyle(mdStyleProps);
-    expect(numberInput).toBeInTheDocument();
-  });
-  it('renders correctly in small size', () => {
-    render(<NumberInput size="sm" aria-label="inputNumber" />);
-
-    const numberInput = screen.getByLabelText('inputNumber');
-
-    expect(numberInput).toHaveStyle(smStyleProps);
     expect(numberInput).toBeInTheDocument();
   });
 });
