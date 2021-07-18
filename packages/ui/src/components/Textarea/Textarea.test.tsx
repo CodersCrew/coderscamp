96cdfd653ef324fff21bf90fae606924c377772a
Cyprian Zamiara <43120132+Veryyapeee@users.noreply.github.com>
2021-07-18 18:34:46 +0200

    feat(ui): Create Textarea component (#33) (#54)
    * #33/Feature/Refactor: Add textarea component, add tests / change prettier config
    
    * #33: Feature: Finish tests for textarea
    
    * #33 Feature: Add padding theme
    
    * #33 Refactor: add theme styles
    
    * #33 Refactor
    
    * fix
    
    * fix conflict
    
    * #33 Refactor: Change base styles
    
    * #33 Refactor: Adjust to PR comments
    
    * #33 Bugfix: fix default borderColor
    
    * #33 fix

---
+++ packages/ui/src/components/Textarea/Textarea.test.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import { Textarea } from './index';
+
+const getTextarea = () => screen.getByRole('textbox');
+
+describe('Textarea', () => {
+  it('Renders correctly in disabled state', () => {
+    render(<Textarea disabled />);
+    expect(getTextarea()).toBeDisabled();
+  });
+  it('Renders correctly as default textarea', () => {
+    render(<Textarea />);
+    expect(getTextarea()).toBeEnabled();
+    expect(getTextarea()).toBeInTheDocument();
+  });
+  it('Renders correctly in invalid state', () => {
+    render(<Textarea invalid />);
+    expect(getTextarea()).toBeInvalid();
+  });
+});
