39708bd9023793a8124038eb8d125d4c095deeff
Natalia <natalia.debska9*@gmail.com>
2021-07-19 23:11:34 +0200

    add button style

---
+++ packages/ui/src/theme/overwrites/components/button.ts
@@ -0,0 +1,39 @@
+export const Button = {
+  sizes: {
+    xs: {
+		py: '4px',
+		svg: {
+        	height: '13px',
+        	width: '13.33px',
+      },
+    },
+    sm: {
+		py: '6px',
+		svg: {
+        	height: '20px',
+        	width: '20px',
+      },
+    },
+    md: {
+		py: '8px',
+		svg: {
+        	height: '24px',
+        	width: '24px',
+      },
+    },
+    lg: {
+		py: '10px',
+		svg: {
+        	height: '28px',
+        	width: '28px',
+      },
+    },
+  },
+  variants: {
+    link: {
+      _hover: {
+        textDecoration: 'none',
+      },
+    },
+  },
+};
--- packages/ui/src/theme/overwrites/components/index.ts
+++ packages/ui/src/theme/overwrites/components/index.ts
@@ -1,7 +1,9 @@
+import { Button } from './button';
 import { Input } from './input';
 import { Radio } from './radio';
 
 export const components = {
+  Button,
   Radio,
   Input,
 };
