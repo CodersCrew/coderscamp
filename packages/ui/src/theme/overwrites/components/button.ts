46501860bb913aa05d5e1b39b3eb5459c89b677d
Natalia <natalia.debska9*@gmail.com>
2021-07-19 23:43:35 +0200

    fix eslint errors

--- packages/ui/src/theme/overwrites/components/button.ts
+++ packages/ui/src/theme/overwrites/components/button.ts
@@ -1,31 +1,31 @@
 export const Button = {
   sizes: {
     xs: {
-		py: '4px',
-		svg: {
-        	height: '13px',
-        	width: '13.33px',
+      py: '4px',
+      svg: {
+        height: '13px',
+        width: '13.33px',
       },
     },
     sm: {
-		py: '6px',
-		svg: {
-        	height: '20px',
-        	width: '20px',
+      py: '6px',
+      svg: {
+        height: '20px',
+        width: '20px',
       },
     },
     md: {
-		py: '8px',
-		svg: {
-        	height: '24px',
-        	width: '24px',
+      py: '8px',
+      svg: {
+        height: '24px',
+        width: '24px',
       },
     },
     lg: {
-		py: '10px',
-		svg: {
-        	height: '28px',
-        	width: '28px',
+      py: '10px',
+      svg: {
+        height: '28px',
+        width: '28px',
       },
     },
   },
