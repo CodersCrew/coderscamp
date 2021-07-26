cb16e35892af4f51d9d3144d6ca3ecfbb298c5a6
Natalia <59486546+talcia@users.noreply.github.com>
2021-07-26 18:23:19 +0200

    refactor(ui): Button component (#9) (#71)
    Co-authored-by: Natalia <natalia.debska9*@gmail.com>

---
+++ packages/ui/src/theme/overwrites/components/button.ts
@@ -0,0 +1,39 @@
+export const Button = {
+  sizes: {
+    xs: {
+      py: '4px',
+      svg: {
+        height: '16px',
+        width: '16px',
+      },
+    },
+    sm: {
+      py: '6px',
+      svg: {
+        height: '20px',
+        width: '20px',
+      },
+    },
+    md: {
+      py: '8px',
+      svg: {
+        height: '24px',
+        width: '24px',
+      },
+    },
+    lg: {
+      py: '10px',
+      svg: {
+        height: '28px',
+        width: '28px',
+      },
+    },
+  },
+  variants: {
+    link: {
+      _hover: {
+        textDecoration: 'none',
+      },
+    },
+  },
+};
--- packages/ui/src/theme/overwrites/components/index.ts
+++ packages/ui/src/theme/overwrites/components/index.ts
@@ -1,9 +1,11 @@
+import { Button } from './button';
 import { Input } from './input';
 import { NumberInput } from './numberInput';
 import { Radio } from './radio';
 import { Textarea } from './textarea';
 
 export const components = {
+  Button,
   Radio,
   NumberInput,
   Textarea,
