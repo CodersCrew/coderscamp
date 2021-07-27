bdc45a246859b2c423875d256ee2e67fedfd617c
KonradSzwarc <konrad.szwarc.dev@gmail.com>
2021-07-27 20:36:23 +0200

    Update Jest configs to correctly calculate test coverage

--- packages/ui/jest.config.ts
+++ packages/ui/jest.config.ts
@@ -5,6 +5,12 @@ import { createJestConfig } from '../../jest.config';
 export default createJestConfig({
   tsconfig: resolve(__dirname, 'tsconfig.json'),
   moduleNameMapper: { '^@/(.+)': '<rootDir>/src/$1' },
+  collectCoverageFrom: [
+    '!**/(theme|icons)/**',
+    '!**/*.(stories|mocks).{ts,tsx}',
+    '!**/index.{ts,tsx}',
+    '!**/components/*.tsx',
+  ],
   overrides: {
     testEnvironment: 'jsdom',
     setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
