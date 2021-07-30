9c3a3bd5b15471cb03aa9e31183d15c40ab66329
KonradSzwarc <konrad.szwarc.dev@gmail.com>
2021-07-30 11:27:19 +0200

    Run lint --fix

--- packages/ui/src/components/MaterialsCard/MaterialsCard.test.tsx
+++ packages/ui/src/components/MaterialsCard/MaterialsCard.test.tsx
@@ -8,22 +8,27 @@ describe('MaterialsCard', () => {
     render(<MaterialsCard status="idle" />);
 
     const button = screen.getByText(MaterialsCardText.idle.buttonText);
+
     expect(button).toBeEnabled();
 
     expect(button).toBeInTheDocument();
   });
+
   it('should renders correctly in loading status', () => {
     render(<MaterialsCard status="loading" />);
 
     const button = screen.getByText(MaterialsCardText.loading.buttonText);
+
     expect(button).toBeDisabled();
 
     expect(button).toBeInTheDocument();
   });
+
   it('should renders correctly in generated status', () => {
     render(<MaterialsCard status="generated" />);
 
     const button = screen.getByText(MaterialsCardText.generated.buttonText);
+
     expect(button).toBeEnabled();
 
     expect(button).toBeInTheDocument();
