8bab21ac3bd44fba100a8ea9a12a3d4c2194d5e4
Krystian <kry.kjjk@op.pl>
2021-08-04 20:51:56 +0200

    Correct export name

--- packages/ui/src/components/TimelineItem/index.ts
+++ packages/ui/src/components/TimelineItem/index.ts
@@ -1,2 +1,2 @@
 export { TimelineItem } from './TimelineItem';
-export type { TimelineItemProps as TimeLineItemProps } from './TimelineItem';
+export type { TimelineItemProps } from './TimelineItem';
