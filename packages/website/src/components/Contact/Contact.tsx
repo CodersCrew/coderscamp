73bbedd52915c6962ebbad76bfe1c7d76c167893
KonradSzwarc <konrad.szwarc.dev@gmail.com>
2021-08-09 20:22:45 +0200

    Replace Flex with the Center component

--- packages/website/src/components/Contact/Contact.tsx
+++ packages/website/src/components/Contact/Contact.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Flex } from '@coderscamp/ui/components/Flex';
+import { Center } from '@coderscamp/ui/components/Center';
 import { VStack } from '@coderscamp/ui/components/Stack';
 
 import { Form } from './Form';
@@ -8,11 +8,11 @@ import { Heading } from './Heading';
 
 export const Contact = () => {
   return (
-    <Flex justify="center" align="center" py={{ base: '40px', sm: '80px' }}>
+    <Center py={{ base: '40px', sm: '80px' }}>
       <VStack width="min(1280px, 100%)" spacing="48px" px={{ base: '24px', sm: '32px', lg: '64px', '2xl': 0 }}>
         <Heading />
         <Form />
       </VStack>
-    </Flex>
+    </Center>
   );
 };
