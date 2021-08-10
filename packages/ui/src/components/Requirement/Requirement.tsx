a727a4714bf339925ac57ee1ce3b6781b9ef4624
kacper-cyra <kacper.cyranowski@gmail.com>
2021-08-10 14:34:20 +0200

    Adjustments

--- packages/ui/src/components/Requirement/Requirement.tsx
+++ packages/ui/src/components/Requirement/Requirement.tsx
@@ -1,16 +1,16 @@
 import React, { ReactText } from 'react';
-import { Flex } from '@chakra-ui/react';
+import { Flex, FlexProps } from '@chakra-ui/react';
 
 import { SolidCheckIcon } from '../../icons/SolidCheck';
 import { Typography } from '../Typography';
 
 export type RequirementProps = {
   children: ReactText;
-};
+} & FlexProps;
 
-export const Requirement = ({ children }: RequirementProps) => {
+export const Requirement = ({ children, ...rest }: RequirementProps) => {
   return (
-    <Flex>
+    <Flex {...rest}>
       <SolidCheckIcon w="48px" h="48px" color="green.500" mr="16px" />
       <Typography color="gray.700" size="lg" weight="normal">
         {children}
--- packages/website/src/components/MentorRequirements.tsx
+++ packages/website/src/components/MentorRequirements.tsx
@@ -1,47 +1,38 @@
 import React from 'react';
 
 import { Box } from '@coderscamp/ui/components/Box';
-import { Flex } from '@coderscamp/ui/components/Flex';
 import { Requirement } from '@coderscamp/ui/components/Requirement';
+import { Stack } from '@coderscamp/ui/components/Stack';
 import { Typography } from '@coderscamp/ui/components/Typography';
 
 export const MentorRequirements = () => {
-  const flexItemMinWidth = { base: '100%', lg: '0px' };
-  const flexItemVerticalMargin = { base: '32px', lg: '0px' };
+  const flexItemVerticalMargin = { base: '16px', lg: '20px' };
 
   return (
     <Box py="80px" mx={{ base: '30px', xl: 'auto' }} maxW="min(1280px, 100%)">
       <Typography mb="64px" textAlign="center" as="h2" size="4xl" weight="extrabold" color="gray.900">
         Czego oczekujemy od mentora
       </Typography>
-      <Box my={{ base: '32px', lg: '64px' }}>
-        <Requirement>
-          Znasz już większość zagadnień poruszanych na kursie i jesteś gotowy nauczyć się reszty. Jeżeli przykładowo
-          pracujesz jako Frontend Developer i nie znasz dobrze Node.js lub jako Backend Developer i nie znasz dobrze
-          React’a, nie przejmuj się tym. Jeśli tylko masz chęć rozwoju w tych dziedzinach, pomożemy Ci nadrobić braki
-          jeszcze przed rozpoczęciem kursu.
+      <Requirement my={{ base: '16px', lg: '64px' }}>
+        Znasz już większość zagadnień poruszanych na kursie i jesteś gotowy nauczyć się reszty. Jeżeli przykładowo
+        pracujesz jako Frontend Developer i nie znasz dobrze Node.js lub jako Backend Developer i nie znasz dobrze
+        React’a, nie przejmuj się tym. Jeśli tylko masz chęć rozwoju w tych dziedzinach, pomożemy Ci nadrobić braki
+        jeszcze przed rozpoczęciem kursu.
+      </Requirement>
+      <Stack direction={{ base: 'column', lg: 'row' }} spacing={flexItemVerticalMargin} wrap="wrap">
+        <Requirement flex="1 1 0px">
+          Posiadasz doświadczenie projektowe w branży IT. Jesteś w stanie poprzeć je ciekawym CV i/lub rozbudowanym
+          portfolio na GitHubie.
         </Requirement>
-      </Box>
-      <Flex wrap="wrap">
-        <Box flex="1 1 0px" minW={flexItemMinWidth} my={flexItemVerticalMargin}>
-          <Requirement>
-            Posiadasz doświadczenie projektowe w branży IT. Jesteś w stanie poprzeć je ciekawym CV i/lub rozbudowanym
-            portfolio na GitHubie.
-          </Requirement>
-        </Box>
-        <Box flex="1 1 0px" mx={{ base: '0px', lg: '40px' }} minW={flexItemMinWidth} my={flexItemVerticalMargin}>
-          <Requirement>
-            Chcesz wspierać innych w stawianiu pierwszych kroków w branży IT. Jesteś gotowy poświęcać na to kilka godzin
-            każdego tygodnia przez 6 miesięcy.
-          </Requirement>
-        </Box>
-        <Box flex="1 1 0px" minW={flexItemMinWidth} my={flexItemVerticalMargin}>
-          <Requirement>
-            Przewodzenie zespołowi oraz uczenie innych sprawiają Ci przyjemność. Potrafisz w prosty sposób przedstawiać
-            trudne zagadnienia.
-          </Requirement>
-        </Box>
-      </Flex>
+        <Requirement flex="1 1 0px">
+          Chcesz wspierać innych w stawianiu pierwszych kroków w branży IT. Jesteś gotowy poświęcać na to kilka godzin
+          każdego tygodnia przez 6 miesięcy.
+        </Requirement>
+        <Requirement flex="1 1 0px">
+          Przewodzenie zespołowi oraz uczenie innych sprawiają Ci przyjemność. Potrafisz w prosty sposób przedstawiać
+          trudne zagadnienia.
+        </Requirement>
+      </Stack>
     </Box>
   );
 };
