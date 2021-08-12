edad8696b4987ab08c97e4b8648ebce67474682c
Krystian <kry.kjjk@op.pl>
2021-08-12 17:30:19 +0200

    Add benefits section with implementation

---
+++ packages/website/src/components/Benefits/BenefitItems.tsx
@@ -0,0 +1,40 @@
+import { SolidMailIcon } from '@coderscamp/ui/icons';
+
+export const benefitItems = [
+  {
+    icon: <SolidMailIcon />,
+    title: 'Atrakcyjne portfolio',
+    subtitle:
+      'Podczas kursu stworzysz i opublikujesz w sieci aż 6 projektów! Nauczysz się także, jak opisywać i zaprezentować każdy z nich, aby Twoje portfolio było atrakcyjne dla rekruterów.',
+  },
+  {
+    icon: <SolidMailIcon />,
+    title: 'Praca zespołowa',
+    subtitle:
+      'W branży IT nigdy nie działasz sam – u nas też nie będziesz. Każdy z projektów zrealizujesz wspólnie z 6-osobowym zespołem, doskonaląc swoje umiejętności pracy w grupie.',
+  },
+  {
+    icon: <SolidMailIcon />,
+    title: 'Wsparcie mentora',
+    subtitle:
+      'Już od pierwszego projektu w Twoim zespole pojawi się mentor – dedykowana danemu zespołowi, doświadczona osoba, która wesprze Cię na każdym etapie rozwoju oraz realizacji projektów.',
+  },
+  {
+    icon: <SolidMailIcon />,
+    title: 'Społeczność',
+    subtitle:
+      'Od początku kursu otrzymujesz dostęp do wewnętrznego komunikatora, na którym możesz zadawać pytania i wymieniać się wiedzą z innymi uczestnikami i mentorami.',
+  },
+  {
+    icon: <SolidMailIcon />,
+    title: 'Interaktywny plan nauki',
+    subtitle:
+      'Wszystkie materiały do nauki zebraliśmy dla Ciebie w postaci interaktywnej checklisty, pozwalającej na odhaczanie przerobionych treści oraz bieżące śledzenie postępów.',
+  },
+  {
+    icon: <SolidMailIcon />,
+    title: 'Różnorodność materiałów',
+    subtitle:
+      'Artykuły, prezentacje, podcasty, filmiki, kursy, gry, interaktywne zadania, sandbox’y – to tylko niektóre spośród kilkudziesięciu form materiałów, z jakimi spotkasz się na kursie.',
+  },
+];
--- packages/website/src/components/Benefits/Benefits.tsx
+++ packages/website/src/components/Benefits/Benefits.tsx
@@ -1,12 +1,36 @@
-import { Flex } from '@coderscamp/ui/components/Flex';
+import { BenefitCard } from '@coderscamp/ui/components/BenefitCard';
+import { Button } from '@coderscamp/ui/components/Button';
+import { Center } from '@coderscamp/ui/components/Center';
+import { VStack } from '@coderscamp/ui/components/Stack';
 import { Typography } from '@coderscamp/ui/components/Typography';
+import { SolidArrowDownIcon } from '@coderscamp/ui/icons';
+
+import { benefitItems } from './BenefitItems';
 
 export const Benefits = () => {
   return (
-    <Flex justify="center" mx="auto" pt="40px" pb="80px" maxW="min(1120px, 100%)">
-      <Typography size="4xl" weight="extrabold">
-        Co wyróżnia CodersCamp?
-      </Typography>
-    </Flex>
+    <Center justify="center" pt="40px" pb="80px" textAlign="center" flexDirection="column" px="12px">
+      <VStack maxWidth="1280px">
+        <Typography size="4xl" weight="extrabold">
+          Co wyróżnia CodersCamp?
+        </Typography>
+        <Center pt="60px" pb="12px" flexWrap="wrap" justifyContent={{ base: 'space-around', xl: 'space-between' }}>
+          {benefitItems.map((benefit) => (
+            <BenefitCard
+              key={benefit.title}
+              {...benefit}
+              width={{ base: 'min(100%, 400px)', md: '320px', lg: '400px' }}
+              backgroundColor="gray.200"
+              my="32px"
+              mx="4px"
+              pb="6px"
+            />
+          ))}
+        </Center>
+        <Button size="lg" color="brand" icon={<SolidArrowDownIcon />}>
+          Rozwiń listę zalet
+        </Button>
+      </VStack>
+    </Center>
   );
 };
