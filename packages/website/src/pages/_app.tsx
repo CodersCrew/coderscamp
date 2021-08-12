45759386fcf8921bc251e00b1ce8a44e59f396f9
KonradSzwarc <konrad.szwarc.dev@gmail.com>
2021-08-12 22:41:45 +0200

    feat(ui): modify ThemeProvider to allow custom global styles per app

--- packages/website/src/pages/_app.tsx
+++ packages/website/src/pages/_app.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from '@coderscamp/ui/theme';
 // eslint-disable-next-line @typescript-eslint/naming-convention
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <ThemeProvider>
+    <ThemeProvider globalStyles={{ 'html, body': { backgroundColor: 'white' } }}>
       <Component {...pageProps} />
     </ThemeProvider>
   );
