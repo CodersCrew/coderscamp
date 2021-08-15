ac0c9f5b06f6a5079d97d2e68ca28bda400986d1
KonradSzwarc <konrad.szwarc.dev@gmail.com>
2021-08-15 15:11:31 +0200

    feat(website): create Testimonials section (#96)

--- packages/website/src/pages/index.tsx
+++ packages/website/src/pages/index.tsx
@@ -3,6 +3,7 @@ import { BigNumbers } from '@/components/BigNumbers/BigNumbers';
 import { Hero } from '@/components/Hero';
 import { LearningSources } from '@/components/LearningSources';
 import { Projects } from '@/components/Projects';
+import { Testimonials } from '@/components/Testimonials';
 
 const Home = () => {
   return (
@@ -12,6 +13,7 @@ const Home = () => {
       <BigNumbers />
       <LearningSources />
       <Projects />
+      <Testimonials />
     </>
   );
 };
