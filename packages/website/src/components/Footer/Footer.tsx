20bff36c9c524d33229a54b931c8123035bad8e4
PiotrWR <pwrynio@gmail.com>
2021-08-23 16:21:38 +0200

    Add react html skeleton

--- packages/website/src/components/Footer/Footer.tsx
+++ packages/website/src/components/Footer/Footer.tsx
@@ -1,8 +1,49 @@
+import { Button } from '@coderscamp/ui/components/Button';
+import { Logo } from '@coderscamp/ui/components/Logo';
+
 export const Footer = () => {
   return (
     <section className="footer">
-      <section className="footer__about">.CodersCrew</section>
-      <section className="footer__nav">Nav</section>
+      <section className="footer__about aboutCodersCrew">
+        <div className=" aboutCodersCrew__container">
+          <Logo color="black" className="aboutCodersCrew__logo" />
+          <div className="aboutCodersCrew__information">
+            Organizatorem CodersCamp jest Stowarzyszenie CodersCrew - organizacja non-profit zrzeszająca pasjonatów
+            dziedzin związanych z IT.
+          </div>
+          <div className="aboutCodersCrew__information">icon_1 icon_2</div>
+        </div>
+      </section>
+
+      <section className="footer__nav footerNav">
+        <div className=" footerNav__container">
+          <div className="footerNav__nav">
+            <header className="footerNav__title"> title</header>
+            <span className="footerNav__item"> item </span>
+            <span className="footerNav__item"> item </span>
+            <span className="footerNav__item"> item </span>
+          </div>
+          <div className="footerNav__toDownload">
+            <header className="footerNav__title"> title</header>
+            <span className="footerNav__item"> item </span>
+            <span className="footerNav__item"> item </span>
+            <span className="footerNav__item"> item </span>
+          </div>
+          <div className="footerNav__legalIssues">
+            <header className="footerNav__title"> title</header>
+            <span className="footerNav__item"> item </span>
+            <span className="footerNav__item"> item </span>
+            <span className="footerNav__item"> item </span>
+          </div>
+        </div>
+      </section>
+
+      <section className="footer__bottomSection">
+        <div className=" footerBottomSection__container">
+          <p className="footerBottomSection__copyright">© CodersCamp 2021, wszelkie prawa zastrzeżone.</p>
+          <Button className="footerBottomSection__copyright">Powered by Vercel</Button>
+        </div>
+      </section>
     </section>
   );
 };
