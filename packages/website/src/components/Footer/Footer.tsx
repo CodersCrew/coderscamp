8657ba7204dae60fb433bc4fad51ef21cf7ce68f
PiotrWR <pwrynio@gmail.com>
2021-08-24 23:20:03 +0200

    Finish styling css

--- packages/website/src/components/Footer/Footer.tsx
+++ packages/website/src/components/Footer/Footer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 
 import { Box } from '@coderscamp/ui/components/Box';
 import { Flex } from '@coderscamp/ui/components/Flex';
@@ -6,24 +7,33 @@ import { Logo } from '@coderscamp/ui/components/Logo';
 import { Typography } from '@coderscamp/ui/components/Typography';
 import { useBreakpointValue } from '@coderscamp/ui/hooks/useBreakpointValue';
 
-import { socialMediaIcons } from './Footer.data';
+import { footerNav, socialMediaIcons } from './Footer.data';
 
 export const Footer = () => {
   const footerStyles = {
     upperFooter: {
-      direction: useBreakpointValue({ base: 'column', lg: 'row' } as const),
+      direction: useBreakpointValue({ base: 'column', xl: 'row' } as const),
     } as const,
     lowerFooter: {
       direction: useBreakpointValue({ base: 'column', md: 'row' } as const),
       order: useBreakpointValue({ base: -1, md: 1 } as const),
     } as const,
+    navSection: {
+      marginLeft: useBreakpointValue({ base: '-80px', xl: '0' } as const),
+    } as const,
   } as const;
 
   return (
     <Flex w="100%" direction="column" align="center" bg="gray.50" color="gray.500">
-      <Flex maxW="1280px" w="80%" direction="column" pt="80px" pb="80px">
-        <Flex w="100%" direction={footerStyles.upperFooter.direction} align="center" justify="space-between">
-          <Box maxW="384px" direction="column" pb="88px">
+      <Flex maxW="1280px" w="80%" direction="column" mt="80px" mb="80px">
+        <Flex
+          w="100%"
+          direction={footerStyles.upperFooter.direction}
+          align="flex-start"
+          justify="space-between"
+          mb={[0, 0, 0, 0, '32px']}
+        >
+          <Box maxW="384px" direction="column" mb="56px">
             <Logo color="black" w="291px" h="32px" />
 
             <Typography w="100%" pt="20px" size="md">
@@ -44,28 +54,24 @@ export const Footer = () => {
             </Flex>
           </Box>
 
-          <Flex maxW="560px" className="footer__nav footerNav" pb="88px">
-            <div className=" footerNav__container">
-              <div className="footerNav__nav">
-                <header className="footerNav__title"> title</header>
-                <span className="footerNav__item"> item </span>
-                <span className="footerNav__item"> item </span>
-                <span className="footerNav__item"> item </span>
-                <span className="footerNav__item"> item </span>
-              </div>
-              <div className="footerNav__toDownload">
-                <header className="footerNav__title"> title</header>
-                <span className="footerNav__item"> item </span>
-                <span className="footerNav__item"> item </span>
-                <span className="footerNav__item"> item </span>
-              </div>
-              <div className="footerNav__legalIssues">
-                <header className="footerNav__title"> title</header>
-                <span className="footerNav__item"> item </span>
-                <span className="footerNav__item"> item </span>
-                <span className="footerNav__item"> item </span>
-              </div>
-            </div>
+          <Flex mb="56px" ml={footerStyles.navSection.marginLeft}>
+            <Flex wrap="wrap">
+              {footerNav.map(({ title, items }) => (
+                <Flex direction="column" key={title} pl="80px" mb="28px">
+                  {/* TODO: Brak czcionki z bold 600*/}
+                  <Typography size="sm" color="gray.400" weight="bold" letterSpacing="0.05em">
+                    {title.toUpperCase()}
+                  </Typography>
+                  {items.map(({ name, urlPath }) => (
+                    <Typography size="md" color="gray.500" key={name}>
+                      <Link key={name} href={urlPath}>
+                        {name}
+                      </Link>
+                    </Typography>
+                  ))}
+                </Flex>
+              ))}
+            </Flex>
           </Flex>
         </Flex>
 
