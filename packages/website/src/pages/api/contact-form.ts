3bb9fd1b27fe7d86578cdf5bc7b13e389a0ecfc8
Konrad Szwarc <konrad.szwarc.dev@gmail.com>
2021-08-24 22:54:39 +0200

    feat(website): add sending email logic to the contact form (#191) (#192)

---
+++ packages/website/src/pages/api/contact-form.ts
@@ -0,0 +1,44 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import nodemailer, { SendMailOptions } from 'nodemailer';
+
+import type { FormValues } from '@/components/Contact/Form';
+
+import { CONTACT_EMAIL } from '../../constants';
+
+async function sendContactForm(req: NextApiRequest, res: NextApiResponse) {
+  const port = Number(process.env.NODEMAILER_PORT);
+  const host = process.env.NODEMAILER_HOST;
+  const user = process.env.NODEMAILER_USER;
+  const pass = process.env.NODEMAILER_PASS;
+
+  if (!port || Number.isNaN(port) || !host || !user || !pass) {
+    return res.status(500).json({ message: 'Environment not configured for sending emails' });
+  }
+
+  if (req.method === 'POST') {
+    const values: FormValues = req.body;
+
+    const transporter = nodemailer.createTransport({ port, host, auth: { user, pass } });
+
+    const mailOptions: SendMailOptions = {
+      from: `${values.fullName} ${values.email}`,
+      to: CONTACT_EMAIL,
+      subject: values.subject,
+      text: values.message,
+      html: `<div>${values.message}</div>`,
+    };
+
+    try {
+      await transporter.sendMail(mailOptions);
+
+      return res.status(200).json({ message: 'E-mail sent successfully' });
+    } catch (ex) {
+      return res.status(500).json({ message: 'Error when sending e-mail' });
+    }
+  }
+
+  return res.status(400).json({ message: `Unsupported request type - ${req.method}` });
+}
+
+// eslint-disable-next-line import/no-default-export
+export default sendContactForm;
