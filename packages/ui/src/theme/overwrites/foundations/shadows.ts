81210a917e94df4f86a8bed0e70811bf5f66b243
Luke_Z <zurawskiu@gmail.com>
2021-08-25 20:12:09 +0200

    feat(website): create recruitment modals (#162) (#183)
    * Add modal header, body, and footer
    
    * Add modals to buttons
    
    * Add modal in the schedule section
    
    * Add support for smaller devices
    
    * Create a useModal hook and add context to handle the recruitment modal.
    
    * Move recruitment modal to its own context
    
    Co-authored-by: KonradSzwarc <konrad.szwarc.dev@gmail.com>

--- packages/ui/src/theme/overwrites/foundations/shadows.ts
+++ packages/ui/src/theme/overwrites/foundations/shadows.ts
@@ -1,4 +1,5 @@
 export const shadows = {
   base: '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
   outline: '0 0 0 3px rgba(56, 189, 248, 0.6)',
+  large: '0 4px 6px -2px rgba(0,0,0,0.05), 0 10px 15px -3px rgba(0,0,0,0.1)',
 };
