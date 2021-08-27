02ce5e267297bb62d835d03367584e8e2ba5647e
KonradSzwarc <konrad.szwarc.dev@gmail.com>
2021-08-27 14:46:03 +0200

    Fix paths in api tests

--- packages/api/jest.config.ts
+++ packages/api/jest.config.ts
@@ -1,10 +1,19 @@
 import { resolve } from 'path';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { pathsToModuleNameMapper } from 'ts-jest/utils';
 
 import { createJestConfig } from '../../jest.config';
+import { compilerOptions } from './tsconfig.json';
+
+const paths = pathsToModuleNameMapper(compilerOptions.paths) as Record<string, string>;
+const moduleNameMapper = Object.keys(paths).reduce<Record<string, string>>(
+  (acc, key) => ({ ...acc, [key]: `<rootDir>/${paths[key]}` }),
+  {},
+);
 
 export default createJestConfig({
   tsconfig: resolve(__dirname, 'tsconfig.json'),
-  moduleNameMapper: { '^@/(.+)': '<rootDir>/src/$1' },
+  moduleNameMapper,
   overrides: {
     collectCoverageFrom: ['**/*.ts'],
   },
