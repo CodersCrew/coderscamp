ebff363ae746f2c9c82470d1b887fd11162c4f37
Luke_Z <zurawskiu@gmail.com>
2021-08-28 21:14:15 +0200

    Refactor MobileBaseNavbar.

--- packages/website/src/components/Navbar/BaseNavbar.tsx
+++ packages/website/src/components/Navbar/BaseNavbar.tsx
@@ -1,12 +1,14 @@
+import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
+import { Box } from '@coderscamp/ui/components/Box';
 import { Flex } from '@coderscamp/ui/components/Flex';
 import { IconButton } from '@coderscamp/ui/components/IconButton';
 import { Logo } from '@coderscamp/ui/components/Logo';
-import { Menu as ChakraMenu, MenuButton, MenuItem, MenuList } from '@coderscamp/ui/components/Menu';
+import { VStack } from '@coderscamp/ui/components/Stack';
 import { useBreakpointValue } from '@coderscamp/ui/hooks/useBreakpointValue';
-import { SolidMenuIcon } from '@coderscamp/ui/icons';
+import { SolidCloseIcon, SolidMenuIcon } from '@coderscamp/ui/icons';
 
 import { NavbarItem } from './NavbarItem';
 
@@ -29,28 +31,6 @@ const NavbarElements = [
   },
 ];
 
-export const MobileBaseNavbar = () => {
-  const logoLayout = useBreakpointValue({ sm: 'square', md: 'horizontal' } as const);
-  const { route } = useRouter();
-  const fontWeight = (link: string) => (link === route ? 'bold' : 'normal');
-
-  return (
-    <Flex alignItems="center">
-      <ChakraMenu>
-        <MenuButton as={IconButton} aria-label="Options" icon={<SolidMenuIcon />} />
-        <MenuList>
-          {NavbarElements.map((element) => (
-            <Link key={element.text} href={element.destinationLink}>
-              <MenuItem fontWeight={fontWeight(element.destinationLink)}>{element.text}</MenuItem>
-            </Link>
-          ))}
-        </MenuList>
-      </ChakraMenu>
-      {logoLayout && <Logo ml="6" maxWidth="280px" color="black" layout={logoLayout} />}
-    </Flex>
-  );
-};
-
 export const DesktopBaseNavbar = () => {
   const logoLayout = useBreakpointValue({ base: 'square', xl: 'horizontal' } as const);
 
@@ -63,5 +43,70 @@ export const DesktopBaseNavbar = () => {
         ))}
       </Flex>
     </>
+  );
+};
+
+export const MobileBaseNavbar = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const logoLayout = useBreakpointValue({ sm: 'square', md: 'horizontal' } as const);
+  const { route } = useRouter();
+  const fontWeight = (link: string) => (link === route ? 'bold' : 'normal');
+
+  const changeDisplayMobileMenu = () => setIsVisible(!isVisible);
+
+  useEffect(() => {
+    if (isVisible) {
+      document.body.style.overflow = 'hidden';
+    }
+
+    return () => {
+      document.body.style.overflow = 'unset';
+    };
+  });
+
+  return (
+    <Flex>
+      <IconButton size="md" aria-label="Open Menu" icon={<SolidMenuIcon />} onClick={changeDisplayMobileMenu} />
+      {isVisible && (
+        <Flex
+          w="100vw"
+          h="100vh"
+          position="fixed"
+          top="0"
+          left="0"
+          zIndex={20}
+          bgColor="white"
+          flexDirection="column"
+          overflow="hidden"
+        >
+          <Flex justifyContent="flex-end">
+            <IconButton
+              mt="18px"
+              mr="8px"
+              aria-label="Close Menu"
+              size="md"
+              icon={<SolidCloseIcon />}
+              onClick={changeDisplayMobileMenu}
+            />
+          </Flex>
+
+          <VStack h="100%" justifyContent="space-evenly">
+            {NavbarElements.map((element) => (
+              <Link key={element.text} href={element.destinationLink}>
+                <Box
+                  as="p"
+                  fontWeight={fontWeight(element.destinationLink)}
+                  fontSize="3xl"
+                  onClick={changeDisplayMobileMenu}
+                >
+                  {element.text}
+                </Box>
+              </Link>
+            ))}
+          </VStack>
+        </Flex>
+      )}
+      {logoLayout && <Logo ml="6" maxWidth="280px" color="black" layout={logoLayout} />}
+    </Flex>
   );
 };
