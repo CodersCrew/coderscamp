9acbdaa6f220a19986ab3e203a5652541325ae36
Krystian <kry.kjjk@op.pl>
2021-08-31 16:28:50 +0200

    fix(website): react headroom - navigation (#202)

--- packages/website/src/components/Navbar/Navbar.tsx
+++ packages/website/src/components/Navbar/Navbar.tsx
@@ -1,5 +1,6 @@
+import Headroom from 'react-headroom';
+
 import { Button } from '@coderscamp/ui/components/Button';
-import { Center } from '@coderscamp/ui/components/Center';
 import { Flex } from '@coderscamp/ui/components/Flex';
 import { HStack } from '@coderscamp/ui/components/Stack';
 import { useBreakpointValue } from '@coderscamp/ui/hooks/useBreakpointValue';
@@ -8,13 +9,22 @@ import { useRecruitmentModal } from '@/components/RecruitmentModal';
 
 import { DesktopBaseNavbar, MobileBaseNavbar } from './BaseNavbar';
 
+const headroomStyles = {
+  zIndex: 1000,
+  background: 'rgba(255, 255, 255)',
+  boxShadow: '1px 1px 1px rgba(0,0,0,0.25)',
+  width: '100%',
+  justifyContent: 'center',
+  display: 'flex',
+} as const;
+
 export const Navbar = () => {
   const baseNavbar = useBreakpointValue({ base: <MobileBaseNavbar />, lg: <DesktopBaseNavbar /> } as const);
   const buttonSize = useBreakpointValue({ base: 'xs', sm: 'md' } as const);
   const { openModal } = useRecruitmentModal();
 
   return (
-    <Center width="100%">
+    <Headroom style={headroomStyles}>
       <HStack
         py="18px"
         px={{ base: '8px', sm: '18px', md: '24px', lg: '40px' }}
@@ -33,6 +43,6 @@ export const Navbar = () => {
           </Button>
         </Flex>
       </HStack>
-    </Center>
+    </Headroom>
   );
 };
