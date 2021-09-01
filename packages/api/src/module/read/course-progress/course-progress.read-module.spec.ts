4310bd0d049e5cb2ca26afa707b8de26366d985e
Mateusz Nowak <kontakt.mateusznowak@gmail.com>
2021-09-01 20:42:06 +0200

    feat(api): extend ApplicationEvent type with global order (#262) (#265)
    * EventRepository returns saved ApplicationEvent on write
    
    * Add test for PrismaEventRepository
    
    * Easier write slice testing api
    
    * Add --detectOpenHandles to code-check.yml

--- packages/api/src/module/read/course-progress/course-progress.read-module.spec.ts
+++ packages/api/src/module/read/course-progress/course-progress.read-module.spec.ts
@@ -3,17 +3,17 @@ import { AsyncReturnType } from 'type-fest';
 import { v4 as uuid } from 'uuid';
 import waitForExpect from 'wait-for-expect';
 
-import { initTestModule } from '@/common/test-utils';
-import { ApplicationEvent } from '@/module/application-command-events';
+import { initReadTestModule } from '@/common/test-utils';
 import { LearningMaterialsUrlWasGenerated } from '@/module/events/learning-materials-url-was-generated.domain-event';
 import { TaskWasCompleted } from '@/module/events/task-was-completed.domain-event';
 import { TaskWasUncompleted } from '@/module/events/task-was-uncompleted-event.domain-event';
+import { StorableEvent } from '@/write/shared/application/event-repository';
 import { EventStreamName } from '@/write/shared/application/event-stream-name.value-object';
 
 const statusTask = (
   learningMaterialsId: string,
   type: 'TaskWasUncompleted' | 'TaskWasCompleted',
-): ApplicationEvent<TaskWasUncompleted | TaskWasCompleted> => {
+): StorableEvent<TaskWasUncompleted | TaskWasCompleted> => {
   return {
     type,
     id: uuid(),
@@ -38,31 +38,31 @@ const givenData = (id: string) => {
 };
 
 async function courseProgressTestModule() {
-  const { prismaService, close, eventOccurred } = await initTestModule();
+  const { prismaService, close, eventOccurred } = await initReadTestModule();
 
   async function expectReadModel(expectation: {
     learningMaterialsId: string;
     readModel: Omit<CourseProgress, 'id'> | null;
   }) {
-    await waitForExpect(() =>
-      expect(
-        prismaService.courseProgress.findUnique({
-          where: { learningMaterialsId: expectation.learningMaterialsId },
-          select: {
-            id: false,
-            courseUserId: true,
-            learningMaterialsCompletedTasks: true,
-            learningMaterialsId: true,
-          },
-        }),
-      ).resolves.toStrictEqual(expectation.readModel),
-    );
+    await waitForExpect(async () => {
+      const readModel = await prismaService.courseProgress.findUnique({
+        where: { learningMaterialsId: expectation.learningMaterialsId },
+        select: {
+          id: false,
+          courseUserId: true,
+          learningMaterialsCompletedTasks: true,
+          learningMaterialsId: true,
+        },
+      });
+
+      expect(readModel).toStrictEqual(expectation.readModel);
+    });
   }
 
   return { eventOccurred, expectReadModel, close };
 }
 
-const learningMaterialsUrlWasGeneratedWithId = (id: string): ApplicationEvent<LearningMaterialsUrlWasGenerated> => {
+const learningMaterialsUrlWasGeneratedWithId = (id: string): StorableEvent<LearningMaterialsUrlWasGenerated> => {
   const SAMPLE_MATERIALS_URL = 'https://app.process.st/runs/jNMTGn96H8Xe3H8DbcpJOg';
   const courseUserId = `userId-${id}`;
 
