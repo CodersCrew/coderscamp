2ada61d15310c4f3e08f4db5ba5d4977d3fe26d5
KonradSzwarc <konrad.szwarc.dev@gmail.com>
2021-09-03 01:58:38 +0200

    feat(website): create the Section component and apply it in all sections

--- packages/website/src/components/Contact/Contact.tsx
+++ packages/website/src/components/Contact/Contact.tsx
@@ -1,18 +1,14 @@
 import React from 'react';
 
-import { Center } from '@coderscamp/ui/components/Center';
-import { VStack } from '@coderscamp/ui/components/Stack';
-
+import { Section } from '../Section';
 import { Form } from './Form';
 import { Heading } from './Heading';
 
 export const Contact = () => {
   return (
-    <Center py={{ base: '40px', sm: '80px' }}>
-      <VStack width="min(1280px, 100%)" spacing="48px" px={{ base: '24px', sm: '32px', lg: '64px', '2xl': 0 }}>
-        <Heading />
-        <Form />
-      </VStack>
-    </Center>
+    <Section spacing="48px" py={{ base: '24px', sm: '80px' }}>
+      <Heading />
+      <Form />
+    </Section>
   );
 };
