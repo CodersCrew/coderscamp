f4c9578f8c1af0dd7e50db24a7901d7055a08ef2
Krystian <kry.kjjk@op.pl>
2021-09-02 20:45:51 +0200

    Update zIndex

--- packages/website/src/components/Navbar/Navbar.tsx
+++ packages/website/src/components/Navbar/Navbar.tsx
@@ -6,20 +6,26 @@ import { Center } from '@coderscamp/ui/components/Center';
 import { Flex } from '@coderscamp/ui/components/Flex';
 import { HStack } from '@coderscamp/ui/components/Stack';
 import { useBreakpointValue } from '@coderscamp/ui/hooks/useBreakpointValue';
+import { useTheme } from '@coderscamp/ui/hooks/useTheme';
 
 import { useRecruitmentModal } from '@/components/RecruitmentModal';
 
 import { DesktopBaseNavbar, MobileBaseNavbar } from './BaseNavbar';
 
 export const Navbar = () => {
+  const { zIndices } = useTheme();
   const [hasShadow, setHasShadow] = useState(false);
   const baseNavbar = useBreakpointValue({ base: <MobileBaseNavbar />, lg: <DesktopBaseNavbar /> } as const);
   const buttonSize = useBreakpointValue({ base: 'xs', sm: 'md' } as const);
   const { openModal } = useRecruitmentModal();
 
   return (
-    <ReactHeadroom onPin={() => setHasShadow(true)} onUnfix={() => setHasShadow(false)}>
-      <Center zIndex="docked" bgColor="white" shadow={hasShadow ? 'large' : undefined}>
+    <ReactHeadroom
+      style={{ zIndex: zIndices.docked }}
+      onPin={() => setHasShadow(true)}
+      onUnfix={() => setHasShadow(false)}
+    >
+      <Center bgColor="white" shadow={hasShadow ? 'large' : undefined}>
         <HStack
           py="18px"
           px={{ base: '8px', sm: '18px', md: '24px', lg: '40px' }}
