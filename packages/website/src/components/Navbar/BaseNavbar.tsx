9e83b33d3b71942129218e89f5d4dbdfd375b592
Luke_Z <zurawskiu@gmail.com>
2021-09-03 20:13:07 +0200

    start work with mobile menu mockup

--- packages/website/src/components/Navbar/BaseNavbar.tsx
+++ packages/website/src/components/Navbar/BaseNavbar.tsx
@@ -1,15 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { Drawer, DrawerBody, DrawerContent, DrawerHeader, DrawerOverlay } from '@chakra-ui/react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
-import { Box } from '@coderscamp/ui/components/Box';
+import { Button } from '@coderscamp/ui/components/Button';
 import { Flex } from '@coderscamp/ui/components/Flex';
 import { IconButton } from '@coderscamp/ui/components/IconButton';
 import { Logo } from '@coderscamp/ui/components/Logo';
 import { VStack } from '@coderscamp/ui/components/Stack';
 import { useBreakpointValue } from '@coderscamp/ui/hooks/useBreakpointValue';
-import { SolidCloseIcon, SolidMenuIcon } from '@coderscamp/ui/icons';
+import { useDisclosure } from '@coderscamp/ui/hooks/useDisclosure';
+import { useMediaQuery } from '@coderscamp/ui/hooks/useMediaQuery';
+import { OutlinedMenuIcon } from '@coderscamp/ui/icons';
 
+import { useRecruitmentModal } from '../RecruitmentModal';
 import { NavbarItem } from './NavbarItem';
 
 const NavbarElements = [
@@ -31,88 +35,75 @@ const NavbarElements = [
   },
 ];
 
-export const DesktopBaseNavbar = () => {
-  const logoLayout = useBreakpointValue({ base: 'square', xl: 'horizontal' } as const);
+export const MobileBaseNavbar = () => {
+  const { route } = useRouter();
+  const fontWeight = (link: string) => (link === route ? 'bold' : 'normal');
+  const [isSmallerThan560px] = useMediaQuery('(max-width: 560px)');
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { openModal } = useRecruitmentModal();
 
   return (
+    <Flex order={2}>
+      <IconButton aria-label="Nawigacja" icon={<OutlinedMenuIcon />} size="md" bg="transparent" onClick={onOpen} />
+      <Drawer placement="left" onClose={onClose} isOpen={isOpen}>
+        <DrawerOverlay />
+        <DrawerContent px="16px">
+          <DrawerHeader pt="31px" pb="41px" px={0}>
+            <Logo maxWidth="250px" color="black" layout="horizontal" />
+          </DrawerHeader>
+          <DrawerBody p={0}>
+            <VStack spacing="20px" alignItems="flex-start">
+              {NavbarElements.map((element) => (
+                <Link key={element.text} href={element.destinationLink}>
+                  <Button
+                    width="100%"
+                    variant="ghost"
+                    weight={fontWeight(element.destinationLink)}
+                    onClick={() => onClose()}
+                  >
+                    {element.text}
+                  </Button>
+                </Link>
+              ))}
+            </VStack>
+            {isSmallerThan560px && (
+              <VStack mt="26px" spacing="16px" width="100%" alignItems="stretch">
+                <Button
+                  size="md"
+                  color="brand"
+                  onClick={() => {
+                    openModal('participant');
+                    onClose();
+                  }}
+                >
+                  Zapisz się na kurs
+                </Button>
+                <Button
+                  size="md"
+                  onClick={() => {
+                    openModal('mentor');
+                    onClose();
+                  }}
+                >
+                  Zostań mentorem
+                </Button>
+              </VStack>
+            )}
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </Flex>
+  );
+};
+
+export const DesktopBaseNavbar = () => {
+  return (
     <>
-      {logoLayout && <Logo maxWidth="280px" color="black" layout={logoLayout} />}
       <Flex>
         {NavbarElements.map((element) => (
           <NavbarItem key={element.text} text={element.text} href={element.destinationLink} />
         ))}
       </Flex>
     </>
-  );
-};
-
-export const MobileBaseNavbar = () => {
-  const [isVisible, setIsVisible] = useState(false);
-  const logoLayout = useBreakpointValue({ sm: 'square', md: 'horizontal' } as const);
-  const { route } = useRouter();
-  const iconButonSize = 'md';
-  const fontWeight = (link: string) => (link === route ? 'bold' : 'normal');
-
-  const changeDisplayMobileMenu = () => setIsVisible(!isVisible);
-
-  useEffect(() => {
-    if (isVisible) {
-      document.body.style.overflow = 'hidden';
-    }
-
-    return () => {
-      document.body.style.overflow = 'unset';
-    };
-  });
-
-  return (
-    <Flex>
-      <IconButton
-        size={iconButonSize}
-        aria-label="Open Menu"
-        icon={<SolidMenuIcon />}
-        onClick={changeDisplayMobileMenu}
-      />
-      {isVisible && (
-        <Flex
-          w="100vw"
-          h="100vh"
-          position="fixed"
-          top="0"
-          left="0"
-          zIndex={20}
-          bgColor="white"
-          flexDirection="column"
-          overflow="hidden"
-        >
-          <Flex justifyContent="flex-end">
-            <IconButton
-              mt="18px"
-              mr="8px"
-              aria-label="Close Menu"
-              size={iconButonSize}
-              icon={<SolidCloseIcon />}
-              onClick={changeDisplayMobileMenu}
-            />
-          </Flex>
-
-          <VStack h="100%" spacing="12">
-            {NavbarElements.map((element) => (
-              <Link key={element.text} href={element.destinationLink}>
-                <Box
-                  as="p"
-                  fontWeight={fontWeight(element.destinationLink)}
-                  fontSize="3xl"
-                  onClick={changeDisplayMobileMenu}
-                >
-                  {element.text}
-                </Box>
-              </Link>
-            ))}
-          </VStack>
-        </Flex>
-      )}
-      {logoLayout && <Logo ml="6" maxWidth="280px" color="black" layout={logoLayout} />}
-    </Flex>
   );
 };
