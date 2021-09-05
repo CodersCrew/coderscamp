f7123021001505090225853377ff37bde41b370d
Kacper Cyranowski <kacper.cyranowski@gmail.com>
2021-09-05 20:23:06 +0200

    controller

---
+++ packages/api/src/module/write/learning-materials-tasks/presentation/rest/process-st-events.rest-controller.ts
@@ -0,0 +1,28 @@
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { CommandBus } from '@nestjs/cqrs';
+
+import { CompleteTaskApplicationCommand } from '@/module/commands/complete-task.application-command';
+import { ApplicationCommandFactory } from '@/write/shared/application/application-command.factory';
+
+import { TaskCompletedRequestBody } from '../../types/taskCompletedRequestBody';
+
+@Controller('process-st/events')
+export class LearningMaterialsUrlRestController {
+  constructor(private readonly commandBus: CommandBus, private readonly commandFactory: ApplicationCommandFactory) {}
+
+  @Post('task-checked-unchecked')
+  @HttpCode(204)
+  async taskCheckedUnchecked(
+    @Body() { id: learningMaterialsId, data: { id: taskId, status } }: TaskCompletedRequestBody,
+  ): Promise<void> {
+    if (status === 'Completed') {
+      const command = this.commandFactory.applicationCommand(() => ({
+        class: CompleteTaskApplicationCommand,
+        type: 'CompleteTask',
+        data: { learningMaterialsId, taskId },
+      }));
+
+      await this.commandBus.execute(command);
+    }
+  }
+}
