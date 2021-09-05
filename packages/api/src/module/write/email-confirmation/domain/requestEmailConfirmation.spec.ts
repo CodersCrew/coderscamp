12c5551796fab67adc3eb4ab21a6a12aafffded5
Krystian <kry.kjjk@op.pl>
2021-09-05 18:04:26 +0200

    Correct test 'Request email confirmation'

--- packages/api/src/module/write/email-confirmation/domain/requestEmailConfirmation.spec.ts
+++ packages/api/src/module/write/email-confirmation/domain/requestEmailConfirmation.spec.ts
@@ -1,9 +1,11 @@
 import { requestEmailConfirmationCommand } from '@/module/commands/request-email-conformation';
+import { emailConfirmationWasRequestedEvent } from '@/module/events/email-confirmation-was-requested.domain-event';
 
 import { requestEmailConfirmation } from './requestEmailConfirmation';
 
 describe('Request email confirmation', () => {
   it('create event EmailConfirmationWasRequested when requestEmailConfirmation is invoked', () => {
+    // given
     const userId = 'ca63d023-4cbd-40ca-9f53-f19dbb19b0ab';
     const confirmationToken = 'sbAPITNMsl2wW6j2cg1H2A';
     const confirmationFor = 'user-registration';
@@ -13,10 +15,13 @@ describe('Request email confirmation', () => {
       confirmationToken,
       confirmationFor,
     });
+
+    // when
     const result = requestEmailConfirmation(command)();
 
-    expect(result).toHaveLength(1);
-    expect(result[0].data).toEqual(command.data);
-    expect(result[0].type).toEqual('EmailConfirmationWasRequested');
+    // then
+    const event = emailConfirmationWasRequestedEvent(command.data);
+
+    expect(result).toEqual([event]);
   });
 });
