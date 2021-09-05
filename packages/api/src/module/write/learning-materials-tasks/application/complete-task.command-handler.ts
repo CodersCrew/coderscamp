90c14bb19aef0b92a729bf18f452b4a77ddcc38b
Kacper Cyranowski <kacper.cyranowski@gmail.com>
2021-09-05 20:30:36 +0200

    command handler

---
+++ packages/api/src/module/write/learning-materials-tasks/application/complete-task.command-handler.ts
@@ -0,0 +1,30 @@
+import { Inject } from '@nestjs/common';
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+
+import { CompleteTaskApplicationCommand } from '@/commands/complete-task.application-command';
+import { TaskWasCompleted } from '@/module/events/task-was-completed.domain-event';
+import { APPLICATION_SERVICE, ApplicationService } from '@/write/shared/application/application-service';
+import { EventStreamName } from '@/write/shared/application/event-stream-name.value-object';
+
+import { completeTask } from '../domain/complete-task';
+
+@CommandHandler(CompleteTaskApplicationCommand)
+export class CompleteTaskCommandHandler implements ICommandHandler<CompleteTaskApplicationCommand> {
+  constructor(
+    @Inject(APPLICATION_SERVICE)
+    private readonly applicationService: ApplicationService,
+  ) {}
+
+  async execute(command: CompleteTaskApplicationCommand): Promise<void> {
+    const eventStream = EventStreamName.from('LearningMaterialsTasks', command.data.learningMaterialsId);
+
+    await this.applicationService.execute<TaskWasCompleted>(
+      eventStream,
+      {
+        causationId: command.id,
+        correlationId: command.metadata.correlationId,
+      },
+      (pastEvents) => completeTask(pastEvents, command),
+    );
+  }
+}
