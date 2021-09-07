ca83677bc804b035f07b81e491b72f2086ffc60c
Kacper Cyranowski <kacper.cyranowski@gmail.com>
2021-09-07 21:28:10 +0200

    code simplifying

--- packages/api/src/module/write/learning-materials-tasks/learning-materials-tasks.test-module.ts
+++ packages/api/src/module/write/learning-materials-tasks/learning-materials-tasks.test-module.ts
@@ -1,5 +1,5 @@
 import { initWriteTestModule } from '@/shared/test-utils';
 
 export async function learningMaterialsTasksTestModule() {
-  return initWriteTestModule((app) => app);
+  return initWriteTestModule();
 }
