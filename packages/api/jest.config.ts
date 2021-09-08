68e069d59d86faf5b68a30fddcce79fd9e2874f8
Mateusz Nowak <mateusz.nowak@vonage.com>
2021-09-08 11:48:17 +0200

    Try to configure name-of

--- packages/api/jest.config.ts
+++ packages/api/jest.config.ts
@@ -10,5 +10,10 @@ export default createJestConfig({
     testTimeout: 6000,
     collectCoverageFrom: ['**/*.ts'],
     setupFiles: ['./jest-setup.ts'],
+    globals: {
+      'ts-jest': {
+        astTransformers: ['ts-nameof'],
+      },
+    },
   },
 });
