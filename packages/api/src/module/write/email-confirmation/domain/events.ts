743db2dff2163dc447705b6784ec21108280855c
Krystian Kijak <45722765+KrystianKjjk@users.noreply.github.com>
2021-09-08 15:28:26 +0200

    feat(api): [write | emailConfirmation] create slice ApproveEmailConfirmation -> EmailConfirmationWasApproved #284 (#313)

--- packages/api/src/module/write/email-confirmation/domain/events.ts
+++ packages/api/src/module/write/email-confirmation/domain/events.ts
@@ -1,3 +1,4 @@
+import { EmailConfirmationWasApproved } from '@/module/events/email-confirmation-was-approved.domain.event';
 import { EmailConfirmationWasRequested } from '@/module/events/email-confirmation-was-requested.domain-event';
 
-export type EmailConfirmationDomainEvent = EmailConfirmationWasRequested;
+export type EmailConfirmationDomainEvent = EmailConfirmationWasRequested | EmailConfirmationWasApproved;
--- packages/api/src/module/write/email-confirmation/email-confirmation.write-module.ts
+++ packages/api/src/module/write/email-confirmation/email-confirmation.write-module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 
 import { SharedModule } from '../shared/shared.module';
+import { ApproveEmailConfirmationApplicationCommandHandler } from './application/approve-email-confirmation-application-command-handler';
 import { RequestEmailConfirmationApplicationCommandHandler } from './application/request-email-confirmation-application-command-handler';
 import { EmailConfirmationRestController } from './presentation/rest/email-confirmation.rest-controller';
 
 @Module({
   imports: [SharedModule],
   controllers: [EmailConfirmationRestController],
-  providers: [RequestEmailConfirmationApplicationCommandHandler],
+  providers: [RequestEmailConfirmationApplicationCommandHandler, ApproveEmailConfirmationApplicationCommandHandler],
 })
 export class EmailConfirmationWriteModule {}
