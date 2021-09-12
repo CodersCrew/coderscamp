3a456d90ad06ed02346126f99172416a80e04fa6
Mateusz Nowak <mateusz.nowak@vonage.com>
2021-09-13 01:24:32 +0200

    Add swagger ui - start

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,6 +1,8 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
 import cookieParser from 'cookie-parser';
+import YAML from 'yamljs';
 
 import { AppModule } from './app.module';
 import { env, validateEnv } from './shared/env';
@@ -16,6 +18,8 @@ async function bootstrap() {
     app.setGlobalPrefix('api');
     app.use(cookieParser(env.COOKIE_SECRET));
     app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+    SwaggerModule.setup('swagger', app, YAML.load('./src/rest-api-docs.yaml'));
 
     await app.listen(env.PORT);
   } catch (ex) {
