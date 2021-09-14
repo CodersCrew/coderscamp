d07b5dcf59309f2c5df750670952c8a40d14e6cd
Mateusz Nowak <mateusz.nowak@vonage.com>
2021-09-14 22:00:01 +0200

    setupFilesAfterEnv with jest-openapi

--- packages/api/jest-setup.ts
+++ packages/api/jest-setup.ts
@@ -1,3 +1,5 @@
+import appRoot from 'app-root-path';
+import jestOpenAPI from 'jest-openapi';
 import { set } from 'leaked-handles';
 
 // detect where are memory leaks in test
@@ -5,4 +7,10 @@ set({
   fullStack: true,
   timeout: 30000,
   debugSockets: true,
+});
+
+export const initOpenApiExpect = () => jestOpenAPI(`${appRoot}/packages/api/rest-api-docs.yaml`);
+
+beforeAll(() => {
+  initOpenApiExpect();
 });
--- packages/api/jest.config.ts
+++ packages/api/jest.config.ts
@@ -9,6 +9,6 @@ export default createJestConfig({
   overrides: {
     testTimeout: 6000,
     collectCoverageFrom: ['**/*.ts'],
-    setupFiles: ['./jest-setup.ts'],
+    setupFilesAfterEnv: ['./jest-setup.ts'],
   },
 });
