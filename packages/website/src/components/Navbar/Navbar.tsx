d01fae32719a7ec012fa6f3bd1f1eafd2c207f97
Krystian Kijak <45722765+KrystianKjjk@users.noreply.github.com>
2021-09-15 12:29:16 +0200

    feat(website): logo is not displayed in Navbar [Safari] #334 (#339)

--- packages/website/src/components/Navbar/Navbar.tsx
+++ packages/website/src/components/Navbar/Navbar.tsx
@@ -2,34 +2,25 @@ import React, { useState } from 'react';
 import ReactHeadroom from 'react-headroom';
 import Link from 'next/link';
 
-import { Button } from '@coderscamp/ui/components/Button';
 import { Center } from '@coderscamp/ui/components/Center';
 import { HStack } from '@coderscamp/ui/components/Stack';
 import { useBreakpointValue } from '@coderscamp/ui/hooks/useBreakpointValue';
-import { useMediaQuery } from '@coderscamp/ui/hooks/useMediaQuery';
 import { useTheme } from '@coderscamp/ui/hooks/useTheme';
 import { LogoBlackHorizontal, LogoBlackSquare } from '@coderscamp/ui/svg/logos';
 
-import { useRecruitmentModal } from '@/components/RecruitmentModal';
-import { MENTOR_RECRUITMENT_FORM_URL } from '@/constants';
-
-import { externalLinkBaseProps } from '../ExternalLink';
 import { DesktopBaseNavbar, MobileBaseNavbar } from './BaseNavbar';
 
+const logoProps = {
+  cursor: 'pointer',
+  maxWidth: '280px',
+  height: '40px',
+};
+
 export const Navbar = () => {
-  const logoLayout = useBreakpointValue({ base: 'square', xl: 'horizontal' } as const);
-  const buttonContainerWidth = useBreakpointValue({ base: '100%', lg: 'fit-content' } as const);
-  const { openModal } = useRecruitmentModal();
-  const [isGreaterThan560px] = useMediaQuery('(min-width: 560px)');
+  const Logo = useBreakpointValue({ base: LogoBlackSquare, xl: LogoBlackHorizontal } as const, 'base');
   const { zIndices } = useTheme();
   const [hasShadow, setHasShadow] = useState(false);
   const baseNavbar = useBreakpointValue({ base: <MobileBaseNavbar />, lg: <DesktopBaseNavbar /> } as const);
-  const baseLogoProps = {
-    cursor: 'pointer',
-    maxWidth: '280px',
-    height: '100%',
-    maxHeight: '40px',
-  };
 
   return (
     <ReactHeadroom
@@ -37,35 +28,17 @@ export const Navbar = () => {
       onPin={() => setHasShadow(true)}
       onUnfix={() => setHasShadow(false)}
     >
-      <Center bgColor="white" shadow={hasShadow ? 'large' : undefined}>
-        <HStack
-          pt={{ base: '32px', lg: '18px' }}
-          pb="18px"
-          px={{ base: '32px', lg: '40px' }}
-          width="min(1920px, 100%)"
-          justifyContent="space-between"
-          alignItems="center"
-          position="relative"
-        >
-          <Link href="/">
-            {logoLayout === 'horizontal' ? (
-              <LogoBlackHorizontal {...baseLogoProps} />
-            ) : (
-              <LogoBlackSquare {...baseLogoProps} />
-            )}
-          </Link>
-
+      <Center
+        pt="18px"
+        pb="18px"
+        px={{ base: '32px', lg: '40px' }}
+        bgColor="white"
+        width="100%"
+        shadow={hasShadow ? 'large' : undefined}
+      >
+        <HStack width="min(1920px, 100%)" justifyContent="space-between">
+          <Link href="/">{Logo && <Logo {...logoProps} />}</Link>
           {baseNavbar}
-          {isGreaterThan560px && (
-            <HStack spacing="12px" pr="24px" width={buttonContainerWidth} justifyContent="flex-end">
-              <Button size="md" color="brand" as="a" href={MENTOR_RECRUITMENT_FORM_URL} {...externalLinkBaseProps}>
-                Zostań mentorem
-              </Button>
-              <Button size="md" onClick={() => openModal('participant')}>
-                Zapisz się na kurs
-              </Button>
-            </HStack>
-          )}
         </HStack>
       </Center>
     </ReactHeadroom>
