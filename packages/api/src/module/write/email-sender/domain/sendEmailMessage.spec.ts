b53cef4fbd7c2ff838877187adefab4aebfee5be
Pawel Szambelan <p.szambelan1@gmail.com>
2021-09-21 16:27:01 +0200

    #261 - add test for creating event

---
+++ packages/api/src/module/write/email-sender/domain/sendEmailMessage.spec.ts
@@ -0,0 +1,34 @@
+import {EmailMessageWasSentDomainEvent} from "@/write/email-sender/domain/events";
+import {SendEmailMessage} from "@/commands/send-email-message.domain-command";
+import {sendEmailMessage} from "@/write/email-sender/domain/sendEmailMessage";
+
+describe('sending email message', () => {
+  it('create event EmailMessageWasSent when SendEmailMessage is invoked after EmailConfirmationWasRequested', () => {
+    // Given
+    const pastEvents: EmailMessageWasSentDomainEvent[] = [];
+
+    const emailData = {
+      emailMessageId: '577e13dd-20ad-4d95-bdd9-d544c73ce358',
+      to: 'test@test.com',
+      subject: 'Test email',
+      text: 'Welcome on board!',
+      html: 'Welcome on board! HTML'
+    }
+    const appEmail = 'coderscamp@gmail.com';
+
+    // When
+    const command: SendEmailMessage = {
+      type: 'SendEmailMessage',
+      data: emailData
+    }
+    const events = sendEmailMessage(pastEvents, command, appEmail);
+
+    // Then
+    expect(events).toStrictEqual([
+      {
+        type: 'EmailMessageWasSent',
+        data: { ...emailData, from: appEmail }
+      }
+    ])
+  })
+});
