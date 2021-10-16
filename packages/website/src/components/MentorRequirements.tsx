2e73392d0a1e2e922188922fffc7f9f00fa95e31
Kamil Dzierzak <kamdzierzak@gmail.com>
2021-10-16 21:39:31 +0200

    try flex 1 1 auto + minHeight

--- packages/website/src/components/MentorRequirements.tsx
+++ packages/website/src/components/MentorRequirements.tsx
@@ -14,28 +14,23 @@ export const MentorRequirements = () => {
       <Typography textAlign="center" as="h2" size="4xl" weight="extrabold" color="gray.900">
         Czego oczekujemy od mentora
       </Typography>
-      <VStack spacing={flexItemVerticalMargin}>
+      <VStack minHeight="min-content" spacing={flexItemVerticalMargin}>
         <Requirement>
           Znasz już większość zagadnień poruszanych na kursie i jesteś gotowy nauczyć się reszty. Jeżeli przykładowo
           pracujesz jako Frontend Developer i nie znasz dobrze Node.js lub jako Backend Developer i nie znasz dobrze
           React’a, nie przejmuj się tym. Jeśli tylko masz chęć rozwoju w tych dziedzinach, pomożemy Ci nadrobić braki
           jeszcze przed rozpoczęciem kursu.
         </Requirement>
-        <Stack
-          height="min-content"
-          direction={{ base: 'column', lg: 'row' }}
-          spacing={flexItemVerticalMargin}
-          wrap="wrap"
-        >
-          <Requirement flex="1 1 0px">
+        <Stack direction={{ base: 'column', lg: 'row' }} spacing={flexItemVerticalMargin} wrap="wrap">
+          <Requirement flex="1 1 auto">
             Posiadasz doświadczenie projektowe w branży IT. Jesteś w stanie poprzeć je ciekawym CV i/lub rozbudowanym
             portfolio na GitHubie.
           </Requirement>
-          <Requirement flex="1 1 0px">
+          <Requirement flex="1 1 auto">
             Chcesz wspierać innych w stawianiu pierwszych kroków w branży IT. Jesteś gotowy poświęcać na to kilka godzin
             każdego tygodnia przez 6 miesięcy.
           </Requirement>
-          <Requirement flex="1 1 0px">
+          <Requirement flex="1 1 auto">
             Przewodzenie zespołowi oraz uczenie innych sprawiają Ci przyjemność. Potrafisz w prosty sposób przedstawiać
             trudne zagadnienia.
           </Requirement>
