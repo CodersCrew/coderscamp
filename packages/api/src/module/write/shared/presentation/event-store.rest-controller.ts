627f7135108e767e9abf6f91ee4245182018f6a8
Mateusz Nowak <mateusz.nowak@vonage.com>
2021-10-26 01:00:39 +0200

    Add specification for server sent events

--- packages/api/src/module/write/shared/presentation/event-store.rest-controller.ts
+++ packages/api/src/module/write/shared/presentation/event-store.rest-controller.ts
@@ -1,9 +1,9 @@
-import { Controller, Get, Inject, Query, Sse } from '@nestjs/common';
-import { EventEmitter2 } from '@nestjs/event-emitter';
-import { Observable } from 'rxjs';
+import {Controller, Get, Inject, Query, Sse} from '@nestjs/common';
+import {EventEmitter2} from '@nestjs/event-emitter';
+import {Observable} from 'rxjs';
 
-import { ApplicationEvent } from '@/module/application-command-events';
-import { EVENT_REPOSITORY, EventRepository, ReadAllFilter } from '@/write/shared/application/event-repository';
+import {ApplicationEvent} from '@/module/application-command-events';
+import {EVENT_REPOSITORY, EventRepository, ReadAllFilter} from '@/write/shared/application/event-repository';
 
 // todo: some guards, filter events by user access
 @Controller('event-store')
@@ -11,7 +11,8 @@ export class EventStoreRestController {
   constructor(
     private readonly eventEmitter: EventEmitter2,
     @Inject(EVENT_REPOSITORY) private readonly eventRepository: EventRepository,
-  ) {}
+  ) {
+  }
 
   @Get('/events')
   async getEvents(
@@ -19,14 +20,25 @@ export class EventStoreRestController {
   ): Promise<{ items: ApplicationEvent[]; metadata: { size: number } }> {
     const events = await this.eventRepository.readAll(query);
 
-    return { items: events, metadata: { size: events.length } };
+    return {items: events, metadata: {size: events.length}};
   }
 
   @Sse('/events')
   // sseOfEvents(@Query() query: Partial<{streamCategory: string}>) {
-  sseOfEvents() {
+  sseOfEvents(): Observable<ServerSentEventResponseBody> {
     return new Observable((observer) => {
-      this.eventEmitter.onAny((event) => observer.next(event));
+      this.eventEmitter.onAny((_, applicationEvent: ApplicationEvent) => observer.next({
+        id: applicationEvent.globalOrder.toString(),
+        data: applicationEvent,
+        type: applicationEvent.type
+      }));
     });
   }
+}
+
+export interface ServerSentEventResponseBody {
+  data: string | object;
+  id?: string;
+  type?: string;
+  retry?: number;
 }
