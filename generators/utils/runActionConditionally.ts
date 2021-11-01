ff2fea90b8bd311c2dd0b4c21d61ab9dd3bcf619
Kacper Cyranowski <kacper.cyranowski@gmail.com>
2021-11-01 21:07:08 +0100

    simplify runActionIfAnswersWereGiven

--- generators/utils/runActionConditionally.ts
+++ generators/utils/runActionConditionally.ts
@@ -6,18 +6,14 @@ import { Answers } from '../types';
 interface ActionInput extends ActionConfig, Partial<Omit<AddActionConfig, 'type'>> {}
 
 // ? skip method omits action if returned value is a string
-export const runActionIfAnswersWereGiven = (promptNames: string[], action: ActionInput, runIfAnswerWasGiven = true) => {
+export const runActionIfAnswersWereGiven = (promptNames: string[], action: ActionInput) => {
   return {
     ...action,
     skip: (answers: Answers) => {
       return promptNames.reduce<boolean | string>((skipAction, promptName) => {
         if (skipAction === 'skip') return 'skip';
 
-        if (runIfAnswerWasGiven) {
-          return answers[promptName] !== '' ? false : 'skip';
-        }
-
-        return answers[promptName] === '' ? false : 'skip';
+        return answers[promptName] !== '' ? false : 'skip';
       }, false);
     },
   };
